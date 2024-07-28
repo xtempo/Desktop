6a74666aad55475db3ace5727bcd8abc5c0bb475
Xtempo <Xtempo@gmail.com>
2024-07-28 11:08:28 +0530

    feed screen

--- Project-Instagram-clone/Instagram-Clone/src/app/(tabs)/index.tsx
+++ Project-Instagram-clone/Instagram-Clone/src/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { Text,View } from "react-native";
+import {FlatList} from "react-native";
+import posts from "~/assets/data/posts.json";
+import PostListItem from "~/src/components/PostListItem";
+
+
 
 
 export default function FeedScreen() {
 
     return (
 
-        <View className="bg-slate-600 flex-1 item-center justify-top m-10">
-            <Text className="text-5xl font-light text-blue-500 text-center">Feed</Text>
-        </View>
+        <FlatList 
+            data={posts}
+            contentContainerStyle={{gap: 20}}
+            renderItem={({ item }) => <PostListItem post={item}/>}
+            showsVerticalScrollIndicator={false}
+        />
     )
+
 }
--- Project-Instagram-clone/Instagram-Clone/src/app/(tabs)/new.tsx
+++ Project-Instagram-clone/Instagram-Clone/src/app/(tabs)/new.tsx
@@ -1,10 +1,14 @@
 import { Stack, Tabs } from "expo-router";
+import { View,Text } from "react-native";
 
 
 export default function CreatePost(){
 
     return (
-        <Tabs>Create Post</Tabs>
+        <View className="bg-pink-500 w-20 aspect-square">
+            <Text className="font-semibold">Hola Gomostas</Text>
+            {/* <Tabs.>open</Tabs.> */}
+        </View>
         
     )
 }
